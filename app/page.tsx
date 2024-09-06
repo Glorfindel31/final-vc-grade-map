4bc88b2dfb93f07b4abedeba405d48655569a6f9
Glorfindel31 <cedric31flo@hotmail.com>
2024-09-07 00:59:52 +0700

    front

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/toggle-mode";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import Link from "next/link";
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1 className="text-4xl font-bold">Hello world</h1>
+        <div className="">
+            <main className="flex flex-col items-center">
+                <div className="h-10 w-full bg-transparent border-b items-center justify-between flex">
+                    <div>
+                        <Button variant="ghost" asChild>
+                            <Link
+                                href="https://www.instagram.com/vietclimb_beta/"
+                                target="_blank"
+                            >
+                                <FaInstagram />
+                            </Link>
+                        </Button>
+                        <Button variant="ghost" asChild>
+                            <Link
+                                href="https://www.facebook.com/VietClimb"
+                                target="_blank"
+                            >
+                                <FaFacebook />
+                            </Link>
+                        </Button>
+                    </div>
+                    <ModeToggle />
+                </div>
             </main>
         </div>
     );
