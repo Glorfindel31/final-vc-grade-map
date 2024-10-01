562c9ba45eebd12beb94686bfbbfd474794f7b25
Glorfindel31 <cedric31flo@hotmail.com>
2024-10-01 11:10:01 +0700

    fixed data table

--- app/routesList/columns.tsx
+++ app/routesList/columns.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { FaPlay } from "react-icons/fa";
+import { TbDropletFilled } from "react-icons/tb";
 
 export interface Route {
     zoneId: number;
@@ -20,6 +20,29 @@ export type RouteData = {
     [key: string]: RouteArray<Route>;
 };
 
+const colorText = {
+    0: " text-pink-500",
+    1: " text-yellow-500",
+    2: " text-orange-500",
+    3: " text-green-500",
+    4: " text-blue-500",
+    5: " text-red-500",
+    6: " text-slate-900",
+    7: " text-slate-300",
+};
+
+const colorBg = {
+    pink: "bg-pink-500",
+    yellow: "bg-yellow-500",
+    orange: "bg-orange-500",
+    green: "bg-green-500",
+    blue: "bg-blue-500",
+    red: "bg-red-500",
+    black: "bg-slate-900",
+    purple: "bg-purple-500",
+    white: "bg-slate-300",
+};
+
 export const columns: ColumnDef<Route>[] = [
     {
         accessorKey: "zoneId",
@@ -32,29 +55,17 @@ export const columns: ColumnDef<Route>[] = [
     },
     {
         accessorKey: "routeColor",
-        header: () => <div className="text-center">Color</div>,
+        header: () => <div className="text-center">Holds</div>,
         cell: ({ row }) => {
-            const routeColor = row.getValue("routeColor") as string;
+            const routeColor: string = row.getValue("routeColor");
+            const color =
+                colorBg[routeColor.toLowerCase() as keyof typeof colorBg];
 
-            if (routeColor === "Black" || routeColor === "White") {
+            if (row.getValue("routeColor")) {
                 return (
                     <div
-                        className={`text-center rounded-2xl w-full ${
-                            routeColor === "Black"
-                                ? "bg-black text-slate-100"
-                                : "bg-slate-100 text-black"
-                        }`}
-                    >
-                        {row.getValue("routeColor")}
-                    </div>
-                );
-            } else {
-                return (
-                    <div
-                        className={`text-center rounded-2xl w-full bg-${routeColor.toLowerCase()}-500`}
-                    >
-                        {row.getValue("routeColor")}
-                    </div>
+                        className={`h-6 w-8 rounded-2xl ${color} outline outline-1`}
+                    ></div>
                 );
             }
         },
@@ -63,28 +74,14 @@ export const columns: ColumnDef<Route>[] = [
         accessorKey: "routeGrade",
         header: () => <div className="text-center ">Grade</div>,
         cell: ({ row }) => {
-            const colorMap = {
-                0: "bg-pink-500",
-                1: "bg-yellow-500 text-yellow-500",
-                2: "bg-orange-500 text-orange-500",
-                3: "bg-green-500 text-green-500",
-                4: "bg-blue-500 text-blue-500",
-                5: "bg-red-500 text-red-500",
-                6: "bg-black text-black",
-                7: "bg-slate-100 text-slate-100",
-            };
-
-            const backgroundColor =
-                colorMap[row.getValue("routeGrade") as keyof typeof colorMap];
-            if (row.getValue("routeGrade")) {
-                return (
-                    <div
-                        className={`text-center rounded-2xl w-full ${backgroundColor}`}
-                    >
-                        {row.getValue("routeGrade") as string}
-                    </div>
-                );
-            }
+            const color =
+                colorText[row.getValue("routeGrade") as keyof typeof colorText];
+            return (
+                <div className={`flex ${color}`}>
+                    <TbDropletFilled />
+                    <TbDropletFilled />
+                </div>
+            );
         },
     },
     {
@@ -96,20 +93,30 @@ export const columns: ColumnDef<Route>[] = [
     },
     {
         accessorKey: "betaLink",
-        header: "Insta Link",
+        header: () => <div className="text-right pr-1">Beta</div>,
         cell: ({ row }) => {
             const instaLink = row.getValue("betaLink") as string;
             if (instaLink) {
                 return (
-                    <Button asChild variant="outline" size="icon">
-                        <Link
-                            href={instaLink}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
+                    <div className="flex justify-end w-full">
+                        <Button asChild variant="outline" size="icon">
+                            <Link
+                                href={instaLink}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaPlay className="w-full" />
+                            </Link>
+                        </Button>
+                    </div>
+                );
+            } else {
+                return (
+                    <div className="flex justify-end w-full">
+                        <Button variant="outline" size="icon" disabled>
                             <FaPlay className="w-full" />
-                        </Link>
-                    </Button>
+                        </Button>
+                    </div>
                 );
             }
         },
